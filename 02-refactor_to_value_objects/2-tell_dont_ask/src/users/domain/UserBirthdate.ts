1f228665557e2b1efbd77f2bd3cfc14c72a8f3a3
Nuria <soriano.nuria@gmail.com>
2023-05-08 10:42:54 +0200

    feat(02-2): tell don't ask step 1

--- 02-refactor_to_value_objects/2-tell_dont_ask/src/users/domain/UserBirthdate.ts
+++ 02-refactor_to_value_objects/2-tell_dont_ask/src/users/domain/UserBirthdate.ts
@@ -1,7 +1,26 @@
 import { DateValueObject } from "../../shared/domain/DateValueObject";
 import { InvalidArgumentError } from "../../shared/domain/InvalidArgumentError";
 
+export enum Generation {
+	Silent,
+	BabyBoomer,
+	GenX,
+	Millennial,
+	GenZ,
+	GenAlpha,
+	Unknown,
+}
+
 export class UserBirthdate extends DateValueObject {
+	private readonly generationConfig = [
+		{ generation: Generation.Silent, minYear: 1928, maxYear: 1945 },
+		{ generation: Generation.BabyBoomer, minYear: 1946, maxYear: 1964 },
+		{ generation: Generation.GenX, minYear: 1965, maxYear: 1980 },
+		{ generation: Generation.Millennial, minYear: 1981, maxYear: 1996 },
+		{ generation: Generation.GenZ, minYear: 1997, maxYear: 2012 },
+		{ generation: Generation.GenAlpha, minYear: 2013 },
+	];
+
 	constructor(readonly value: Date) {
 		super(value);
 
@@ -10,6 +29,21 @@ export class UserBirthdate extends DateValueObject {
 
 	toPrimitives(): Date {
 		return this.value;
+	}
+
+	generation(): Generation {
+		const birthYear = this.value.getFullYear();
+
+		for (const config of this.generationConfig) {
+			if (
+				birthYear >= config.minYear &&
+				(config.maxYear === undefined || birthYear <= config.maxYear)
+			) {
+				return config.generation;
+			}
+		}
+
+		return Generation.Unknown;
 	}
 
 	private ensureIsValidBirthdate(value: Date): void {
