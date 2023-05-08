e82412745de3383f861a0adfe439ff3ae81f282d
Nuria <soriano.nuria@gmail.com>
2023-05-08 10:38:10 +0200

    feat(02-2): tell don't ask initial state

---
+++ 02-refactor_to_value_objects/2-tell_dont_ask/src/users/domain/User.ts
@@ -0,0 +1,23 @@
+import { UserBirthdate } from "./UserBirthdate";
+import { UserEmail } from "./UserEmail";
+import { UserId } from "./UserId";
+
+export class User {
+	private email: UserEmail;
+	private readonly id: UserId;
+	private readonly birthdate: UserBirthdate;
+
+	constructor(id: string, email: string, birthdate: Date) {
+		this.id = new UserId(id);
+		this.email = new UserEmail(email);
+		this.birthdate = new UserBirthdate(birthdate);
+	}
+
+	get emailValue(): string {
+		return this.email.value;
+	}
+
+	updateEmail(newEmail: string): void {
+		this.email = new UserEmail(newEmail);
+	}
+}
